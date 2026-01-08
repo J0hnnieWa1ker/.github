8d385e9c056291bfa2ba69eb03010553e907e7a0
j0hnniewa1ker <email@j0hnniewa1ker.com>
2026-01-08 08:46:02 -0600

    Ellsworth County Medical Center - IT Manager #8

--- profile/README.md
+++ profile/README.md
@@ -1,65 +1,69 @@
-# docker run --rm j0hnniewa1ker/readme:docker > README.md                       
-```                                                                             
-        _  ___  _                 _                    _ _                      
-       (_)/ _ \| |__  _ __  _ __ (_) _____      ____ _/ | | _____ _ __         
-       | | | | | '_ \| '_ \| '_ \| |/ _ \ \ /\ / / _` | | |/ / _ \ '__|  
-       | | |_| | | | | | | | | | | |  __/\ V  V / (_| | |   <  __/ |           
-      _/ |\___/|_| |_|_| |_|_| |_|_|\___| \_/\_/ \__,_|_|_|\_\___|_|     
-     |__/                                                                       
-```                                                                             
+`docker run --rm j0hnniewa1ker/readme:ecmc > README.md`                         
+# [J0hnniewa1ker](https://www.linkedin.com/in/j0hnniewa1ker/)                   
 ## Summary                                                                      
-  - I want to work at Docker                                                    
-- Inspiration from [scratch](https://hub.docker.com/_/scratch) this resume      
-  - endeavors to "Write clear, accurate, well-structured, and consistent technical content from scratch."                                                       
-- Championing Workshops-as-Containers and `hello-world` style newsletters       
-- This role appears to be the place to spend more of my time on improving demos:
-  - [a1](https://hub.docker.com/r/j0hnniewa1ker/a1)                             
-  - [scratch](https://github.com/J0hnnieWa1ker/containers/tree/main/scratch)    
-  - [oc-pwsh](https://github.com/J0hnnieWa1ker/oc-pwsh)                         
-  - [addload](https://github.com/client-engineering-devops/ocp/tree/customer/addload)
- - SVG files make great diagrams in README.md files in git repos.               
-![SVG](https://raw.githubusercontent.com/client-engineering-devops/ocp/customer/svg/oc.svg)
+- After moving back to Kansas I lost motivation in competing in a high stress environment so I stepped back and focused on gardening. So a supportive, family-like culture sounds very attractive.                                     
+## Recommendations                                                              
+- Jon would be a great fit for any technical leadership role. His ability to solve problems, methodically tackle large projects, teach those who are less tenured, and his drive to always keep learning have always impressed me and will be sure to bring value to any future employers.
+  - Andrew 
+- I strongly recommend Jon for any engineering leadership role. He brings a blend of technical depth, strategic thinking, and people-first leadership.
+  - Mike 
+- His balanced approach and collaborative mindset elevate every project he touches.
+  - Steve 
+- Jon’s leadership in this engagement demonstrated that he can absorb technical requirements and turn them into strategies that moves teams forward.
+  - Chris 
+- more here: https://www.linkedin.com/in/j0hnniewa1ker/details/recommendations  
 ## Experience                                                                   
-### IBM                                                 June 2021 - October 2023
-Client Engineering Technology Engineer                                          
-- Mentored clients and colleagues great practices around learning new skills    
-- Created content for learning Cloud Native approaches                          
+### IBM
+#### Client Engineering Technology Engineer
+6/2021 - 10/2023
+- Mentored clients and colleagues great practices around learning new skills.   
+- Wrote comprehensive workshops on Containers, Openshift, and Ansible           
 --------------------------------------------------------------------------------
-### Starbucks                                          September 2016 - May 2021
-Application Developer                                                           
+ 
+### Starbucks
+#### Application Developer
+9/2016 - 5/2021
 - Nginx Project built with Chef Automate including Kitchen and Inspec validation
   - Wrote workshops on Chef basics and nginx endpoint management using Automate 
-  - Presented this workshop to DevOps team                                      
+  - Presented workshops to several teams throughout Starbucks                   
 - OpenStack project focused on improving image delivery to edge devices         
-- Packer qemu_builder project to manage changes to VM images                    
+- Packer qemu_builder project to manage changes to IoT images                   
 - Lead Performance Test team                                                    
-  - lead internal team standups, I also joined Dev and Test standups            
+  - lead internal team standups, I also joined Developer standups               
   - wrote stories and managed team Kanban board                                 
   - pushed for code to be converted to .Net core                                
-  - deployed 20,000 docker containers daily to test IoT Hub application         
+    - we deployed 20,000 docker containers daily to test Azure IoT Hub solution 
 --------------------------------------------------------------------------------
-### Efinancial                                    November 2014 - September 2016
-Devops Engineer                                                                 
-- Built dev, test and pre-producion environments in AWS via PowerShell          
+ 
+### Efinancial
+#### DevOps Engineer
+11/2014 - 9/2016
+- Developed PowerShell Module to deploy environments in AWS                     
 - Upgraded TFS Server and mentored team how to use Release Manager              
 --------------------------------------------------------------------------------
-### Adobe (contact)                                     June 2013 - October 2014
-Release Engineer                                                                
+ 
+### Adobe (contract)
+#### Release Engineer
+6/2013 - 10/2014
 - Migrated legacy build systems to Jenkins and mentored teams how to manage them
-- Developed WinPE to automate Windows deployments as VMs or on bare metal       
+- Developed WinPE to automate Windows deployments                               
+- Developed DMG images to automate OSX deployments                              
 --------------------------------------------------------------------------------
-### Microsoft (contract)                               February 2013 - June 2013
-Build Engineer                                                                  
+ 
+### Microsoft (contract)
+#### Build Engineer
+2/2013 - 6/2013
 - Responsible for build automation, scripting, and release implementation       
-- Validated deployments passed QA before releasing to Pre-Production|Production 
+- Validated deployments passed QA before releasing HR and Staffing systems      
 --------------------------------------------------------------------------------
+ 
 ```                                                                             
 gcc -o readme --static -g readme.c                                              
-docker build . -t j0hnniewa1ker/readme:docker                                   
-docker push j0hnniewa1ker/readme:docker                                         
-git checkout -b docker                                                          
+docker build . -t j0hnniewa1ker/readme:ecmc                                     
+docker push j0hnniewa1ker/readme:ecmc                                           
+git checkout -b ecmc                                                            
 git add README.md readme.c                                                      
-git commit -m "Docker - Technical Writer #4"                                  
+git commit -m "Ellsworth County Medical Center - IT Manager #8"               
 git push                                                                        
 ```                                                                             
-[Docker-Technical Writer #4](https://github.com/J0hnnieWa1ker/.github/issues/4) 
+[ECMC - IT Manager #8](https://github.com/J0hnnieWa1ker/.github/issues/8)       
--- profile/readme.c
+++ profile/readme.c
@@ -1,95 +1,134 @@
 #include <stdio.h>
 
-const char header[] =
-"`docker run --rm j0hnniewa1ker/readme:docker > README.md`                       \n"
-"```                                                                             \n"
-"        _  ___  _                 _                    _ _                      \n"
-"       (_)/ _ \\| |__  _ __  _ __ (_) _____      ____ _/ | | _____ _ __         \n"
-"       | | | | | '_ \\| '_ \\| '_ \\| |/ _ \\ \\ /\\ / / _` | | |/ / _ \\ '__|  \n"
-"       | | |_| | | | | | | | | | | |  __/\\ V  V / (_| | |   <  __/ |           \n"
-"      _/ |\\___/|_| |_|_| |_|_| |_|_|\\___| \\_/\\_/ \\__,_|_|_|\\_\\___|_|     \n"
-"     |__/                                                                       \n"
-"```                                                                             \n"
-"";
-const char summary[] =
-"## Summary                                                                      \n"
-"  - I want to work at Docker                                                    \n"
-"- Inspiration from [scratch](https://hub.docker.com/_/scratch) this resume      \n"
-"  - endevers to \"Write clear, accurate, well-structured, and consistent technical"
-" content from scratch.\"                                                        \n"
-"- Championing Workshops-as-Containers and `hello-world` style newsletters       \n"
-"- This role appears to be the place to spend more of my time on improving demos:\n"
-"  - [a1](https://hub.docker.com/r/j0hnniewa1ker/a1)                             \n"
-"  - [scratch](https://github.com/J0hnnieWa1ker/containers/tree/main/scratch)    \n"
-"  - [oc-pwsh](https://github.com/J0hnnieWa1ker/oc-pwsh)                         \n"
-"  - [addload](https://github.com/client-engineering-devops/ocp/tree/customer/addload)\n"
-" - SVG files make great diagrams in README.md files in git repos.               \n"
-"![SVG](https://raw.githubusercontent.com/client-engineering-devops/ocp/customer/svg/oc.svg)\n"
-"## Experience                                                                   \n"
-"";
-const char ibm[] =
-"### IBM                                                 June 2021 - October 2023\n"
-"Client Engineering Technology Engineer                                          \n"
-"- Mentored clients and colleagues great practices around learning new skills    \n"
-"- Created content for learning Cloud Native approaches                          \n"
-"--------------------------------------------------------------------------------\n"
-"";
-const char starbucks[] =
-"### Starbucks                                          September 2016 - May 2021\n"
-"Application Developer                                                           \n"
-"- Nginx Project built with Chef Automate including Kitchen and Inspec validation\n"
-"  - Wrote workshops on Chef basics and nginx endpoint management using Automate \n"
-"  - Presented this workshop to DevOps team                                      \n"
-"- OpenStack project focused on improving image delivery to edge devices         \n"
-"- Packer qemu_builder project to manage changes to VM images                    \n"
-"- Lead Performance Test team                                                    \n"
-"  - lead internal team standups, I also joined Dev and Test standups            \n"
-"  - wrote stories and managed team Kanban board                                 \n"
-"  - pushed for code to be converted to .Net core                                \n"
-"  - deployed 20,000 docker containers daily to test IoT Hub application         \n"
-"--------------------------------------------------------------------------------\n"
-"";
-const char efinancial[] =
-"### Efinancial                                    November 2014 - September 2016\n"
-"Devops Engineer                                                                 \n"
-"- Built dev, test and pre-producion environments in AWS via PowerShell          \n"
-"- Upgraded TFS Server and mentored team how to use Release Manager              \n"
-"--------------------------------------------------------------------------------\n"
-"";
-const char adobe[] =
-"### Adobe (contact)                                     June 2013 - October 2014\n"
-"Release Engineer                                                                \n"
-"- Migrated legacy build systems to Jenkins and mentored teams how to manage them\n"
-"- Developed WinPE to automate Windows deployments as VMs or on bare metal       \n"
-"--------------------------------------------------------------------------------\n"
-"";
-const char microsoft[] =
-"### Microsoft (contract)                               February 2013 - June 2013\n"
-"Build Engineer                                                                  \n"
-"- Responsible for build automation, scripting, and release implementation       \n"
-"- Validated deployments passed QA before releasing to Pre-Production|Production \n"
-"--------------------------------------------------------------------------------\n"
-"";
-const char process[] =
-"```                                                                             \n"
-"gcc -o readme --static -g readme.c                                              \n"
-"docker build . -t j0hnniewa1ker/readme:docker                                   \n"
-"docker push j0hnniewa1ker/readme:docker                                         \n"
-"git checkout -b docker                                                          \n"
-"git add README.md readme.c                                                      \n"
-"git commit -m \"Docker - Technical Writer #4\"                                  \n"
-"git push                                                                        \n"
-"```                                                                             \n"
-"[Docker-Technical Writer #4](https://github.com/J0hnnieWa1ker/.github/issues/4) \n"
-"";
-int main() {
-  printf("%s",header);
-  printf("%s",summary);
-  printf("%s",ibm);
-  printf("%s",starbucks);
-  printf("%s",efinancial);
-  printf("%s",adobe);
-  printf("%s",microsoft);
-  printf("%s",process);
-  return 0;
+int main(){
+    struct Company
+    {
+        char name[25];
+        char address[50];
+        char phone[15];
+    };
+
+    struct Position
+    {
+        char title[40];
+        char start[10];
+        char end[10];
+        char description[1000];
+    };
+ 
+    const char summary[] =
+    "`docker run --rm j0hnniewa1ker/readme:ecmc > README.md`                         \n"
+    "# [J0hnniewa1ker](https://www.linkedin.com/in/j0hnniewa1ker/)                   \n"
+    "## Summary                                                                      \n"
+    "- After moving back to Kansas I lost motivation in competing in a high stress "
+    "environment so I stepped back and focused on gardening. So a supportive, "
+    "family-like culture sounds very attractive.                                     \n"
+    "## Recommendations                                                              \n"
+    "- Jon would be a great fit for any technical leadership role. His ability to solve"
+    " problems, methodically tackle large projects, teach those who are less tenured, "
+    "and his drive to always keep learning have always impressed me and will be sure "
+    "to bring value to any future employers.\n"
+    "  - Andrew \n"
+    "- I strongly recommend Jon for any engineering leadership role. He brings a blend "
+    "of technical depth, strategic thinking, and people-first leadership.\n"
+    "  - Mike \n"
+    "- His balanced approach and collaborative mindset elevate every project he "
+    "touches.\n"
+    "  - Steve \n"
+    "- Jon’s leadership in this engagement demonstrated that he can absorb technical "
+    "requirements and turn them into strategies that moves teams forward.\n"
+    "  - Chris \n"
+    "- more here: https://www.linkedin.com/in/j0hnniewa1ker/details/recommendations  \n"
+    "## Experience                                                                   \n"
+    "";
+    printf("%s",summary);
+
+    struct Company ibm = {"IBM",
+                          "1 New Orchard Road, Armonk, NY 10504",
+                          "1-800-426-4968" };
+    struct Position client = {"Client Engineering Technology Engineer","6/2021","10/2023",
+        "- Mentored clients and colleagues great practices around learning new skills.   \n"
+        "- Wrote comprehensive workshops on Containers, Openshift, and Ansible           \n"
+        "--------------------------------------------------------------------------------\n"
+    };
+    printf("### %s\n",ibm.name);
+    printf("#### %s\n",client.title);
+    printf("%s - %s\n",client.start,client.end);
+    printf("%s \n", client.description);
+ 
+    struct Company sbux = {"Starbucks",
+                           "2401 Utah Avenue South, Seattle, WA 98134",
+                           "1-206-318-7100" };
+    struct Position dev = {"Application Developer","9/2016","5/2021",
+        "- Nginx Project built with Chef Automate including Kitchen and Inspec validation\n"
+        "  - Wrote workshops on Chef basics and nginx endpoint management using Automate \n"
+        "  - Presented workshops to several teams throughout Starbucks                   \n"
+        "- OpenStack project focused on improving image delivery to edge devices         \n"
+        "- Packer qemu_builder project to manage changes to IoT images                   \n"
+        "- Lead Performance Test team                                                    \n"
+        "  - lead internal team standups, I also joined Developer standups               \n"
+        "  - wrote stories and managed team Kanban board                                 \n"
+        "  - pushed for code to be converted to .Net core                                \n"
+        "    - we deployed 20,000 docker containers daily to test Azure IoT Hub solution \n"
+        "--------------------------------------------------------------------------------\n"
+    };
+    printf("### %s\n",sbux.name);
+    printf("#### %s\n",dev.title);
+    printf("%s - %s\n",dev.start,dev.end);
+    printf("%s \n", dev.description);
+
+    struct Company aliss = {"Efinancial",
+                            "1621 114th Ave SE Ste 118, Bellevue, WA 98015",
+                            "1-800-482-6616" };
+    struct Position devops = {"DevOps Engineer","11/2014","9/2016",
+        "- Developed PowerShell Module to deploy environments in AWS                     \n"
+        "- Upgraded TFS Server and mentored team how to use Release Manager              \n"
+        "--------------------------------------------------------------------------------\n"
+    };
+    printf("### %s\n",aliss.name);
+    printf("#### %s\n",devops.title);
+    printf("%s - %s\n",devops.start,devops.end);
+    printf("%s \n", devops.description);
+
+    struct Company adbe = {"Adobe (contract)",
+                           "801 N 34th St, Seattle, WA 98103",
+                           "1-408-536-6000" };
+    struct Position release = {"Release Engineer","6/2013","10/2014",
+        "- Migrated legacy build systems to Jenkins and mentored teams how to manage them\n"
+        "- Developed WinPE to automate Windows deployments                               \n"
+        "- Developed DMG images to automate OSX deployments                              \n"
+        "--------------------------------------------------------------------------------\n"
+    };
+    printf("### %s\n",adbe.name);
+    printf("#### %s\n",release.title);
+    printf("%s - %s\n",release.start,release.end);
+    printf("%s \n", release.description); 
+
+    struct Company msft = {"Microsoft (contract)",
+                           "One Microsoft Way, Redmond, WA 98052",
+                           "1-425-882-8080" };
+    struct Position build = {"Build Engineer","2/2013","6/2013",
+        "- Responsible for build automation, scripting, and release implementation       \n"
+        "- Validated deployments passed QA before releasing HR and Staffing systems      \n"
+        "--------------------------------------------------------------------------------\n"
+    };
+    printf("### %s\n",msft.name);
+    printf("#### %s\n",build.title);
+    printf("%s - %s\n",build.start,build.end);
+    printf("%s \n", build.description);
+ 
+    const char process[] =
+    "```                                                                             \n"
+    "gcc -o readme --static -g readme.c                                              \n"
+    "docker build . -t j0hnniewa1ker/readme:ecmc                                     \n"
+    "docker push j0hnniewa1ker/readme:ecmc                                           \n"
+    "git checkout -b ecmc                                                            \n"
+    "git add README.md readme.c                                                      \n"
+    "git commit -m \"Ellsworth County Medical Center - IT Manager #8\"               \n"
+    "git push                                                                        \n"
+    "```                                                                             \n"
+    "[ECMC - IT Manager #8](https://github.com/J0hnnieWa1ker/.github/issues/8)       \n"
+    "";
+    printf("%s",process);
+    return 0;
 }
